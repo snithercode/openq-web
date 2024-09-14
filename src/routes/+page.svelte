a7cf13432367fbdda3c40c870ccb925fe4011909
Drew Haile <dhaile.dev@gmail.com>
2024-09-13 23:02:21 -0400

    Tailwind setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<h1 class="text-3xl font-bold underline">
+Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</h1>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+</style>
